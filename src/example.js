f2f60c31028f1e9fa5e3f80b6f1d8bbed4e5394e
emadbaqeri <embaqeri1998@gmail.com>
2021-05-15 04:38:38 +0430

    husky is doing awesome but she is noughty

---
+++ src/example.js
@@ -0,0 +1,13 @@
+const username = "freddy";
+typeof username === "string";
+
+const newLocal = "serviceWorkder";
+if (!(newLocal in navigator)) {
+  console.log(navigator);
+}
+
+const greeting = "hello";
+
+console.log(`${greeting} world`);
+
+[1, 2, 3].forEach((x) => console.log(x));
